630ceabb04a9e9f2716c8a78b92b5a6733bcfb1d
Santiago Spina <santiagospina025@gmail.com>
2023-08-30 16:05:16 +0200

    v1

---
+++ js/index.js
@@ -0,0 +1,61 @@
+let turno = 0
+const tablero = []
+
+
+
+
+
+const btnPulsado = (e, pos) => {
+    turno ++
+    const btn = e.target
+    const color = turno % 2 ? "salmon" : "lawngreen"
+    btn.style.backgroundColor = color
+    tablero[pos] = color
+
+    if(hasWon()){
+        alert(`Player ${color} ha ganado!`)
+    }
+}
+
+const hasWon = () => {
+    //Opciones para ganar//
+
+    //Horizontales
+    if(tablero[0] == tablero[1] && tablero[0] == tablero[2] && tablero[0]) {
+        return true
+    }
+    else if(tablero[3] == tablero[4] && tablero[3] == tablero[5] && tablero[3]){
+        return true
+    }
+    else if(tablero[6] == tablero[7] && tablero[6] == tablero[8] && tablero[6]){
+        return true
+    }
+
+    //Verticales
+    else if(tablero[0] == tablero[3] && tablero[0] == tablero[6] && tablero[0]){
+        return true
+    }
+    else if(tablero[1] == tablero[4] && tablero[1] == tablero[7] && tablero[1]){
+        return true
+    }
+    else if(tablero[2] == tablero[5] && tablero[2] == tablero[8] && tablero[2]){
+        return true
+    }
+    //Diagonales
+    else if(tablero[0] == tablero[4] && tablero[0] == tablero[8] && tablero[0]){
+        return true
+    }
+    else if(tablero[6] == tablero[4] && tablero[6] == tablero[2] && tablero[6]){
+        return true
+    }
+    //Si aun no se ganó
+    else{
+        return false
+    }
+}
+
+
+
+document.querySelectorAll("button").forEach((btn, i)=>{
+    btn.addEventListener("click", (e)=> btnPulsado(e, i))
+})
